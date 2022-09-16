1bcaf520f628274af2861c06d9f286db57e17da1
Aida <shakharova.aida@yandex.ru>
2022-09-16 17:07:15 +0600

    Написать программу копирования массива

---
+++ 3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Написать программу копирования массива
+
+int Counter(string str, char symbol)
+{
+    int count = 0;
+    for(int i=0; i<str.Length; i++)
+        if(str[i] == symbol)
+            count++;
+    return count;
+}
+
+string [] CopyStr(string str, char symbol)
+{
+    string[] coordinates = new string[Counter(str,symbol)+1];
+    int amount=0;
+    for(int i=0;i<str.Length;i++)
+    {
+        if (str[i] == symbol)
+        {
+            amount++;
+            continue;
+        }
+
+        coordinates[amount] = coordinates[amount] + str[i];
+    }
+    return coordinates;
+}
+
+void PrintArray(string[] array)
+{
+    for(int i=0;i<array.Length;i++) 
+    Console.Write($"{array[i]} ");
+}
+
+Console.Write("Введите элементы массива через пробел: ");
+string array = Console.ReadLine();
+string[] copyArray = CopyStr(array, ' ');
+Console.WriteLine("Копия массива создана и имеет название copyArray: ");
+PrintArray(copyArray);
